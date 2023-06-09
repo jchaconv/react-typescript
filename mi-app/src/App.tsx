c4cb07c4db55001ae31d20b6f860e544a75b189a
Julio Cesar Chacon Vilela <julio.chacon@neoris.com>
2023-06-08 22:40:21 -0500

    fin useState

--- mi-app/src/App.tsx
+++ mi-app/src/App.tsx
@@ -1,9 +1,16 @@
+import { Counter } from "./components/Counter";
+import { Usuario } from "./components/Usuario";
 
 
 function App() {
     return (
       <>
         <h1>React - TypeScript</h1>
+        <hr />
+
+        <Counter />
+        <br />
+        <Usuario />
 
       </>
     );
---
+++ mi-app/src/components/Usuario.tsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+
+interface User {
+    uid: string;
+    name: string;
+}
+
+const tempUser: User = {
+    uid: 'Qwertty',
+    name: 'TempUser'
+}
+
+export const Usuario = () => {
+
+    const [user, setUser] = useState<User>(tempUser);
+
+    const login = () => {
+        setUser({
+            uid: 'ABC1213',
+            name: 'Julio Chacon'
+        });
+    }
+
+    return (
+        <div>
+            <h3>Usuario: useState</h3>
+
+            <button
+                onClick={ login }
+                className='btn btn-outline-primary mt-2'
+            >
+                Login
+
+            </button>
+
+            {
+                (!user)
+                    ? <pre className="mt-2">No hay usuario</pre>
+                    : <pre className="mt-2">{ JSON.stringify( user )}</pre>
+            }
+
+        </div>
+    )
+}
